0cc391c9c2b9d8404060d025b8a4a9574a3fad3f
activemax30 <activemax30@gmail.com>
2020-12-12 19:41:53 +0200

    added basic pages with hardcode

--- src/app/pages/car-details/car-details.component.ts
+++ src/app/pages/car-details/car-details.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-car-details',
@@ -7,9 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CarDetailsComponent implements OnInit {
 
-  constructor() { }
+  constructor(private readonly router: Router) { }
 
   ngOnInit(): void {
   }
-
+  goBack() {
+    // this.router.navigate(['/car-list'])
+  }
 }
