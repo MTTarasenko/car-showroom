73f2157fbb6d4ce6c8e9d5a16613d01878e2644c
activemax30 <activemax30@gmail.com>
2020-12-22 17:23:08 +0200

    links fix in header

--- src/app/components/main-header/main-header.component.ts
+++ src/app/components/main-header/main-header.component.ts
@@ -1,12 +1,11 @@
 import {Component, OnInit} from '@angular/core';
-import {NavigationEnd, Router} from '@angular/router';
-import {filter, switchMap} from 'rxjs/operators';
+import { Router} from '@angular/router';
 import {MatDialog} from '@angular/material/dialog';
-import {Observable} from 'rxjs';
 
 import {AuthGuardService} from '../../guards/auth-guard.service';
 import {AddCarModalComponent} from '../add-car-modal/add-car-modal.component';
 import {ServerEmulatorService} from '../../services/server-emulator.service';
+import {switchMap} from 'rxjs/operators';
 
 @Component({
   selector: 'app-main-header',
@@ -15,7 +14,6 @@ import {ServerEmulatorService} from '../../services/server-emulator.service';
 })
 export class MainHeaderComponent implements OnInit {
 
-  public isOnCarDetails$: Observable<boolean>;
 
   constructor(public readonly router: Router,
               private authService: AuthGuardService,
@@ -25,24 +23,6 @@ export class MainHeaderComponent implements OnInit {
 
   ngOnInit(): void {
 
-    if (this.router.isActive('car-details', false)) {
-      this.isOnCarDetails$ = new Observable<boolean>(
-        observer => {
-          return observer.next(true);
-        }
-      );
-    }
-
-    this.router.events.pipe(filter(event => event instanceof NavigationEnd))
-      .subscribe((event: NavigationEnd) => {
-        this.isOnCarDetails$ = new Observable(observer => {
-          if (event.url.includes('car-details')) {
-            return observer.next(true);
-          } else {
-            return observer.next(false);
-          }
-        });
-      });
   }
 
   addNewCar(): void {
