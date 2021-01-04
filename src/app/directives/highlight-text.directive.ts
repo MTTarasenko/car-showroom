2b66a387422627c86006282eb5bebc85c33ae342
activemax30 <activemax30@gmail.com>
2021-01-04 18:02:05 +0200

    added highlighting pipe

--- src/app/directives/highlight-text.directive.ts
+++ src/app/directives/highlight-text.directive.ts
@@ -1,14 +1,23 @@
-import {Directive, ElementRef, Input} from '@angular/core';
+import {AfterViewInit, Directive, ElementRef, Input} from '@angular/core';
 
 @Directive({
   selector: '[appHighlightText]'
 })
-export class HighlightTextDirective {
+export class HighlightTextDirective implements AfterViewInit {
 
-  @Input() highLightColor: string;
-  constructor(el: ElementRef) {
-    console.log(this.highLightColor);
-    el.nativeElement.style.backgroundColor = this.highLightColor;
+  @Input('appHighlightText') highLightText: string;
+
+  constructor(private el: ElementRef) {
   }
 
+  ngAfterViewInit(): void {
+    console.log('string is: ' + this.el.nativeElement.innerText);
+    const regex = new RegExp(this.highLightText, 'gi');
+    const match = this.el.nativeElement.innerText.match(regex);
+
+    if (match) {
+      this.el.nativeElement.innerHTML = this.el.nativeElement.innerText
+        .replace(regex, `<span class='highlightText'>${match}</span>`);
+    }
+  }
 }
